64304f4a172c79c9e7bb72644fa08f1cddbe65c8
Elisha-Kiplangat <elishakiplanga@gmail.com>
2024-06-29 00:26:50 +0300

    add

--- src/components/AddComment.tsx
+++ src/components/AddComment.tsx
@@ -1,17 +1,17 @@
 
+import { useState } from 'react'
 import image from '../assets/avatars/image-amyrobson.png'
+// import { initialComments } from './Comments/CommentList'
 
  
 
 const AddComment = () => {
+
+    const [comment, setComment] = useState<string>()
     
-    const handleSubmit = (e: any) => {
-        e.preventDefault()
-        const form = e.currentTarget
-        const formData = new FormData(form)
-        const comment = formData.get('comment') as string
-        console.log(comment)
-    }
+      const handleSubmit = (e:any) => {
+        e.preventDefault();
+    };
 
   return (
     <>
@@ -23,6 +23,9 @@ const AddComment = () => {
                 <textarea
                     className="add-comment__content flex-grow border border-gray-300 p-2 rounded-md"
                     placeholder="Write your comment here"
+                    value={comment}
+                    onChange={(e) => setComment(e.target.value)}
+                    
                 ></textarea>
                 <button className="add-comment__submit bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 px-4 rounded ml-2 h-10" type="submit">
                     Send
